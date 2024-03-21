2ec2b1fd13499822cbd7278c2e8baa77a5fef3fc
ROBOT <tkata12@outlook.jp>
2024-03-21 16:15:33 +0900

    v1.0.4

---
+++ dist/index.d.ts
@@ -0,0 +1,20 @@
+type options = (content: string) => string;
+declare function set(...func: options[]): void;
+declare function defaultTemplate(content: string): string;
+declare function defaultFrameTemplate(content: string): string;
+declare const _default: {
+    set: typeof set;
+    defaultTemplate: typeof defaultTemplate;
+    defaultFrameTemplate: typeof defaultFrameTemplate;
+    colors: {
+        black: (content: string) => string;
+        red: (content: string) => string;
+        green: (content: string) => string;
+        yellow: (content: string) => string;
+        blue: (content: string) => string;
+        magenta: (content: string) => string;
+        cyan: (content: string) => string;
+        white: (content: string) => string;
+    };
+};
+export default _default;
