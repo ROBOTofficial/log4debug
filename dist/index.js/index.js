c9262f70b8f52cc1d1358d0b00f13b1d9d8ce5ea
ROBOT <tkata12@outlook.jp>
2024-03-21 15:44:54 +0900

    v1.0.2

---
+++ dist/index.js/index.js
@@ -0,0 +1,50 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function set(...func) {
+    let newConsole = console.log;
+    console.log = (...data) => {
+        let content = data.join(" ");
+        if (func.length !== 0)
+            for (let i = func.length - 1; -1 < i; i--)
+                content = func[i](content);
+        newConsole(content);
+    };
+}
+function defaultTemplate(content) {
+    let date = new Date;
+    let year = date.getFullYear();
+    let month = date.getMonth() - 1;
+    let day = date.getDay();
+    let time = date.getTime();
+    return `${magenta(`[${year}-${month}-${day}:${time}]`)}${green("[LOG]")}: ${content}`;
+}
+function defaultFrameTemplate(content) {
+    let line = "";
+    for (let i = 0; i < content.split("").length + 4; i++)
+        line += "-";
+    return `${line}\n| ${content} |\n${line}`;
+}
+const black = (content) => `\x1b[30m${content}\x1b[0m`;
+const red = (content) => `\x1b[31m${content}\x1b[0m`;
+const green = (content) => `\x1b[32m${content}\x1b[0m`;
+const yellow = (content) => `\x1b[33m${content}\x1b[0m`;
+const blue = (content) => `\x1b[34m${content}\x1b[0m`;
+const magenta = (content) => `\x1b[35m${content}\x1b[0m`;
+const cyan = (content) => `\x1b[36m${content}\x1b[0m`;
+const white = (content) => `\x1b[37m${content}\x1b[0m`;
+const colors = {
+    black,
+    red,
+    green,
+    yellow,
+    blue,
+    magenta,
+    cyan,
+    white
+};
+exports.default = {
+    set,
+    defaultTemplate,
+    defaultFrameTemplate,
+    colors
+};
---
+++ index.ts
@@ -0,0 +1,50 @@
+type options = (content:string) => string
+function set(...func:options[]) {
+    let newConsole = console.log
+    console.log = (...data: any[]) => {
+        let content = data.join(" ")
+        if (func.length !== 0) for (let i = func.length-1; -1 < i; i--) content = func[i](content)
+        newConsole(content)
+    }
+}
+function defaultTemplate(content:string):string {
+    let date = new Date
+    let year = date.getFullYear()
+    let month = date.getMonth()-1
+    let day = date.getDay()
+    let time = date.getTime()
+    return `${magenta(`[${year}-${month}-${day}:${time}]`)}${green("[LOG]")}: ${content}`
+}
+function defaultFrameTemplate(content:string):string {
+    let line = ""
+    for (let i = 0; i < content.split("").length+4; i++) line += "-"
+    return `${line}\n| ${content} |\n${line}`
+}
+
+
+const black = (content:string) => `\x1b[30m${content}\x1b[0m`
+const red = (content:string) => `\x1b[31m${content}\x1b[0m`
+const green = (content:string) => `\x1b[32m${content}\x1b[0m`
+const yellow = (content:string) => `\x1b[33m${content}\x1b[0m`
+const blue = (content:string) => `\x1b[34m${content}\x1b[0m`
+const magenta = (content:string) => `\x1b[35m${content}\x1b[0m`
+const cyan = (content:string) => `\x1b[36m${content}\x1b[0m`
+const white = (content:string) => `\x1b[37m${content}\x1b[0m`
+const colors = {
+    black,
+    red,
+    green,
+    yellow,
+    blue,
+    magenta,
+    cyan,
+    white
+}
+
+export default {
+    set,
+    defaultTemplate,
+    defaultFrameTemplate,
+
+    colors
+}
